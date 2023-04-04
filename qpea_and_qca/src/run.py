05702f3ccc973bde0d886f2147930eb0af9e274e
AMOUZOU Grâce D. A <grace.dorcas.amouzou@gmail.com>
2023-04-04 12:04:25 +0000

    Quick fix on the run file

--- qpea_and_qca/src/run.py
+++ qpea_and_qca/src/run.py
@@ -1,18 +1,28 @@
 from qpea import *
 
-# For test running
-test = True
 
-if test:
-    nb_qubits = 3
-    case = "Z"
-    n_second_register = 1
-    run(nb_qubits, n_second_register, case, test)
-else:
-    nb_qubits = 3
-    n_second_register = 1
-    while nb_qubits < 6:
-        for case in ["Z", "G"]:
-            run(nb_qubits, n_second_register, case, test)
-        clear = True
-        nb_qubits += 1
+def tests(n, test=True):
+    """ Runs the tests. A default setting is set for simple tests. But, all the calculations may be made by putting the
+     variable "test" to false.
+
+    :param int n: The number of qubits.
+    :param bool test : If activated, simples tests will be run.
+
+    """
+    if test:
+        n = 3
+        case = "Z"
+        n_second_register = 1
+        run(n, n_second_register, case, test=True)
+    else:
+        n = 3
+        n_second_register = 1
+        while n < 6:
+            for case in ["Z", "G"]:
+                run(n, n_second_register, case, test=False)
+            clear = True  # To separate the different figures
+            n += 1
+
+
+# For test running
+tests(3)
